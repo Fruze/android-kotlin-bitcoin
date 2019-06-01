d06d4a18c369062d47a2eef4faf99113576c8d28
Andrea <lk.andrea@ymail.com>
2019-06-01 10:25:46 +0700

    Try permission

--- app/src/main/java/com/lkand/bitcoin_tracker/MainActivity.kt
+++ app/src/main/java/com/lkand/bitcoin_tracker/MainActivity.kt
@@ -2,11 +2,77 @@ package com.lkand.bitcoin_tracker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.androidnetworking.AndroidNetworking
+import okhttp3.*
+import okio.ByteString
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val client = OkHttpClient.Builder()
+            .readTimeout(0, TimeUnit.MILLISECONDS)
+            .build()
+
+        val request = Request.Builder()
+            .url("wss://ws-feed.pro.coinbase.com")
+            .build()
+
+//        client.newWebSocket(request, bitcoinRateListener())
+    }
+
+    private fun bitcoinRateListener(): WebSocketListener {
+        return object: WebSocketListener() {
+
+            override fun onOpen(webSocket: WebSocket, response: Response) {
+                super.onOpen(webSocket, response)
+
+                webSocket.send("""
+                    {
+                        "type": "subscribe",
+                        "product_ids": [
+                            "BTC-USD"
+                        ],
+                        "channels": [
+                            "matches"
+                        ]
+                    }
+                """.trimIndent())
+                webSocket.close(10000, "hehe")
+            }
+
+            override fun onMessage(webSocket: WebSocket, bytes: ByteString) {
+                super.onMessage(webSocket, bytes)
+
+                Log.e("byte", bytes.toString())
+            }
+
+            override fun onMessage(webSocket: WebSocket, text: String) {
+                super.onMessage(webSocket, text)
+
+                Log.e("string", text)
+            }
+
+            override fun onClosed(webSocket: WebSocket, code: Int, reason: String) {
+                super.onClosed(webSocket, code, reason)
+
+                webSocket.close(10000, null)
+                runOnUiThread(object: Runnable {
+                    override fun run() {
+                        Log.e("run", "close")
+                    }
+                })
+            }
+
+            override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+                super.onFailure(webSocket, t, response)
+                t.printStackTrace()
+            }
+
+        }
     }
 }
